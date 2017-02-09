d1dd8e03cdbb37aa21821a1c01eb54a8d4ca2f27
Jaspinder <jaspinder@ucreate.co.in>
2017-02-09 14:42:37 +0530

    sdsd

--- app/routes.ts
+++ app/routes.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './components/about/about.component';
 
 export const routes: Route[] = [
      { path: '', pathMatch: 'full', component: LoginComponent },
-    { path: 'home', pathMatch: 'full', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    // { path: 'home', pathMatch: 'full', component: HomeComponent },
+    // { path: 'about', component: AboutComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes, { useHash: true });
